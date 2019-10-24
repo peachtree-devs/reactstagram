08bc9fa3e17c806806381319f65a9db4ba2e30bf
Rick Young <rederic@hudsonmx.com>
2019-10-24 03:39:37 -0400

    styles only branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.scss';
-import Header from './components/Header/Header';
-import Content from './components/Content/Content';
-import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App-Phone">
-      <Header></Header>
-      <Content></Content>
-      <Footer></Footer>
+
     </div>
   );
 }
